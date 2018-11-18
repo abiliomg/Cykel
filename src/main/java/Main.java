650f555621e816028db47788507ffac5db546d5f
Abílio <abilio_mg_castro@hotmail.com>
2018-11-18 01:25:08 +0000

    gradle alterado e inicio de testes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import Exceptions.UserAlreadyExists;
+import Exceptions.UserDoesNotExists;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("ola alteraçao");
+    public static void main(String[] args) throws UserDoesNotExists, UserAlreadyExists {
+        BikeRentalSystem Sistema=new BikeRentalSystem(1);
+        Sistema.registerUser(1,"um",1);
+        Sistema.registerUser(2,"dois",2);
+        Sistema.registerUser(3,"tres",3);
+        Sistema.addCredit(1,1);
+        Sistema.addCredit(2,2);
+        Sistema.addBicycle(1,1,1);
+        Sistema.addBicycle(2,1,2);
+        int returna=Sistema.getBicycle(3,2,1);
+        System.out.println(returna);
     }
 }
--- src/test/java/Testes.java
+++ src/test/java/Testes.java
@@ -1,5 +1,24 @@
+import Exceptions.UserAlreadyExists;
+import Exceptions.UserDoesNotExists;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class Testes{
 
+    @Test
+    public void getBicycleTest() throws UserAlreadyExists, UserDoesNotExists {
+        BikeRentalSystem Sistema=new BikeRentalSystem(1);
+        Sistema.registerUser(1,"um",1);
+        Sistema.registerUser(2,"dois",2);
+        Sistema.registerUser(3,"tres",3);
+        Sistema.addCredit(1,1);
+        Sistema.addCredit(2,2);
+        Sistema.addBicycle(1,1,1);
+        Sistema.addBicycle(2,1,2);
+        Sistema.getBicycle(2,2,0);
+        Sistema.getBicycle(1,1,-1);
+
+    }
 }
