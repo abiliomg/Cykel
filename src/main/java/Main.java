9f4c9cb5effc5da5d1bce255d08ced3e970bdc96
Ricardo <Dumbrica@hotmail.com>
2018-11-19 13:25:06 +0000

    registerUser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,15 @@ import Exceptions.UserAlreadyExists;
 import Exceptions.UserDoesNotExists;
 public class Main {
     public static void main(String[] args) throws UserDoesNotExists, UserAlreadyExists {
-        int x,y;
+
+        /*int x,y;
 
         x = 1;
-        y = 2;
+        y = 2;*/
 
-        /*
+
         BikeRentalSystem  Sistema=new BikeRentalSystem(3);
-      Sistema.registerUser(1,"Ricardo",1);
-      Sistema.registerUser(1,"Ricardo",1);*/
-
-        System.out.println(x/y);
+        Sistema.registerUser(0,"Ricardo",1);
+        Sistema.addCredit(1,10);
     }
 }
