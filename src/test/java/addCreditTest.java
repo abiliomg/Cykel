6eb119b4b1cf9eac2fee23d665f2fa65ed9c6aa3
Abílio <abilio_mg_castro@hotmail.com>
2018-11-19 11:11:31 +0000

    addCreditTest inacabado

---
+++ src/test/java/addCreditTest.java
@@ -0,0 +1,47 @@
+import Exceptions.UserAlreadyExists;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+public class addCreditTest {
+    BikeRentalSystem sistema;
+    @BeforeEach
+    void init() throws UserAlreadyExists {
+        sistema = null;
+        sistema=new BikeRentalSystem(3);
+        sistema.registerUser(0,"teste",1);
+    }
+    @Test
+    public void testCase1(){
+        sistema.addCredit(0,0);
+        assertEquals(false,sistema.verifyCredit(0));
+    }
+    @Test
+    public void testCase2(){
+        sistema.addCredit(0,1);
+        assertEquals(true,sistema.verifyCredit(0));
+    }
+
+    @Test
+    public void testCase3(){
+        sistema.addCredit(0,2);
+        assertEquals(true,sistema.verifyCredit(0));
+    }
+    @Test
+    public void testCase4(){
+        sistema.addCredit(-1,1);
+        assertEquals(false,sistema.verifyCredit(-1));
+    }
+    @Test
+    public void testCase5(){
+        sistema.addCredit(0,1);
+        assertEquals(true,sistema.verifyCredit(0));
+    }
+    @Test
+    public void testCase6(){
+        sistema.addCredit(1,1);
+        assertEquals(1,sistema.getUsers().get(0).getCredit());
+    }
+}
